1ef2c53b301bed25367ac6a6cc98edd887f5a56c
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-04-24 22:42:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function manuOn(){
+    const manu=document.getElementById("manu-item");
+    if (manu.style.display != "block") {
+        manu.style.display="block";
+    } else {
+        manu.style.display="none"; 
+    }
+}
